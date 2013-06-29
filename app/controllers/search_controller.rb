d2c7882c2a4c6ab48ef93fd7149a121d701cc172
Benji Decker <bensdecker@gmail.com>
2013-06-29 15:55:25 -0400

    search fundamentals up
    wow, think may have gottent the basics?

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,27 @@
 class SearchController < ApplicationController
   def results
+
+  	# archive the query for data intelligence
   	@query = Query.new
   	@query.text = params[:search]
   	@query.save
 
-  	@results = User.search params[:search]
+  	# retrieve direct results
+  	@direct_results = User.search params[:search]
+
+
+  	# determine relevancy category ### hacked for now...
+  	@category_id = 1
+
+  	binding.pry
+
+  	# retrieve relevancy results
+  	@relevancies = Relevancy.where("category_id = ?", @category_id).order("score DESC").limit(5)  	
+  	@relevancy_results = []
+  	@relevancies.each {|relevancy| @relevancy_results << relevancy.user_id}
+
+
+  	### will also want to eager load user associations... including whatever USER info will be needed to populate member previews?  does that meean you'll have to pull this for everyone, or just the 5 identified?
+
   end
 end
