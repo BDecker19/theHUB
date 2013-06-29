fa385cba57a9d9b0c0d69de6dbbd621dbcbb72ed
Benji Decker <bensdecker@gmail.com>
2013-06-29 16:46:42 -0400

    gonna try some front end

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,21 +7,18 @@ class SearchController < ApplicationController
   	@query.save
 
   	# retrieve direct results
-  	@direct_results = User.search params[:search]
+  	@results = User.search params[:search]
 
 
   	# determine relevancy category ### hacked for now...
   	@category_id = 1
-
-  	binding.pry
 
   	# retrieve relevancy results
   	@relevancies = Relevancy.where("category_id = ?", @category_id).order("score DESC").limit(5)  	
   	@relevancy_results = []
   	@relevancies.each {|relevancy| @relevancy_results << relevancy.user_id}
 
-
-  	### will also want to eager load user associations... including whatever USER info will be needed to populate member previews?  does that meean you'll have to pull this for everyone, or just the 5 identified?
+  	### will also want to eager load user associations... including whatever USER info will be needed to populate member previews?  don't believe that means you need to pull for everyone, just the ones identified?
 
   end
 end
