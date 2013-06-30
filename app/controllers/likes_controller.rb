24f172b1306679f40abd8a7628826fb0e1693066
Benji Decker <bensdecker@gmail.com>
2013-06-30 09:28:24 -0400

    small finish on previous

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,23 @@
 class LikesController < ApplicationController
  	
-  # POST  ...attribute variables set via 'constructor' gem.  '.type' varibles reference subclass, not parent (for parent, use ".class.model_name")
+  # POST
   def create
     @object_acted_on = params[:acted_on_type].constantize.find(params[:acted_on_id])
 
-    # create new like ...### read you're not supposed to overwrite initialize method?
+    # create new like ###read you're not supposed to overwrite initialize method?
     @like = Like.new
     @like.set_vals({:liker => current_user, :liked => @object_acted_on})
-    binding.pry
     @like.save
 
-    # create action  (relevancy_value = custom value associated with like action ###should prb be converted to class variable -- eg http://bit.ly/15RgiuK)
-    @action = Action.new :actor_id => current_user.id, :action_product_id => @like.id, :action_product_type => "like", :acted_on_id => @object_acted_on.id, :acted_on_type => @object_acted_on.type, :relevancy_value => 1
+    # create action
+    @action = Action.new 
+    @action.set_vals({:doer => current_user, :product => @like, :done_to => @object_acted_on, :relevancy_value => 1}) ### relevancy value will become lookup against the action_relevancy_value table
 
     # update or create relevancy
         #check if user already has relevancy for category
         if(!current_user.relevancies.select{|relevancy| relevancy.category_id == @object_acted_on.category_id}.empty?)
             # if relevancy found...
-            @relevancy = Relevancy.where("user_id = ? AND category_id = ?", current_user.id, @object_acted_on.category_id)[0]  ### should also have check to confirm only 1 result found
+            @relevancy = Relevancy.where("user_id = ? AND category_id = ?", current_user.id, @object_acted_on.category_id)[0]  ### should also have check to confirm only 1 result found?
             @relevancy.score += @action.relevancy_value
         else
             # if no relevancy found... 
--- app/models/action.rb
+++ app/models/action.rb
@@ -13,7 +13,7 @@ class Action < ActiveRecord::Base
 
 		self.actor_id = params[:doer].id
 		self.action_product_id = params[:product].id
-		self.action_product_type = params[:product].class
+		self.action_product_type = params[:product].class.to_s
 		self.acted_on_id = params[:done_to].id
 		self.acted_on_type = params[:done_to].type  #subclass(eg ContentItemArticle), not parent(eg ContentItem). For parent, use ".class.model_name"
 		self.relevancy_value = params[:relevancy_value]
